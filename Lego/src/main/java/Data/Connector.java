3d67fdceb8a0a43a030761b7c40f38b3c7baad56
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-03-19 14:25:50 +0100

    basic login

---
+++ Lego/src/main/java/Data/Connector.java
@@ -0,0 +1,32 @@
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * The purpose of Connector is to...
+ *
+ * @author kasper
+ */
+public class Connector {
+
+    private static final String URL = "jdbc:mysql://207.154.240.48:3306/LegoDB";
+    private static final String USERNAME = "guest";
+    private static final String PASSWORD = "1234";
+
+    private static Connection singleton;
+
+    public static void setConnection(Connection con) {
+        singleton = con;
+    }
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        if (singleton == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return singleton;
+    }
+
+}
